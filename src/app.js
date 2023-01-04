eec3cd985339a583a9c1ac257a87767733700fd0
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-01-03 22:58:39 -0300

    sign-up route with controller

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-import express from "express"
+import express, { json } from "express"
 import cors from "cors"
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
+app.use(cors())
+app.use(json())
+
+
+let users = []
+let tweets = []
+
+app.post("/sign-up", (req, res) => {
+
+   const user = req.body 
+
+   users.push(user)
+
+   res.sendStatus(201)
+})
+
+app.get("/", (req, res) => {
+
+    res.send(users)
+})
 
 
 
