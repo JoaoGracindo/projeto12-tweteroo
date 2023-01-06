1ecf78eaa5eba2b65fe7fc1eb878873020b45e7b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-01-06 12:11:38 -0300

    user validation fix

--- src/schemas.js
+++ src/schemas.js
@@ -1,9 +1,13 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
-    username: Joi.required(),
+    username: Joi.required()
+                 .string()
+                 .min(1),
     avatar: Joi.required()
-}) ;
+                .string()
+                .min(1)
+});
 
 const tweetSchema = Joi.object({
     username: Joi.required(),
